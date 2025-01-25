b2a0d5036bb19c7e35a4adb57a029c47801bf311
Evgenii Klepilin <evgenii.klepilin@gmail.com>
2025-01-25 17:07:23 -0600

    Add exit builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
   "strings"
+  "strconv"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -19,6 +20,24 @@ func main() {
 
     if err != nil {
       fmt.Fprintln(os.Stderr, "Error reading input: ", err)
+      os.Exit(1)
+      break
+    }
+
+    fields := strings.Fields(input)
+
+    if fields[0] == "exit" {
+      if len(fields) > 1 {
+        num, errConv := strconv.Atoi(fields[1])
+        if errConv != nil {
+          fmt.Fprintln(os.Stderr, "Invalid error code", errConv)
+          continue
+        }
+        os.Exit(num)
+      } else {
+        os.Exit(0)
+      }
+      break
     }
 
     fmt.Println(strings.TrimSuffix(input, "\n") + ": command not found")
