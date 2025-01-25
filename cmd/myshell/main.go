c14adce089d59ce949930e230fe37043d23e9846
Evgenii Klepilin <evgenii.klepilin@gmail.com>
2025-01-25 16:38:27 -0600

    Refactor with the solution

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -14,7 +14,11 @@ func main() {
   fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	var input, _ = bufio.NewReader(os.Stdin).ReadString('\n')
+	var input, err = bufio.NewReader(os.Stdin).ReadString('\n')
 
-  fmt.Fprint(os.Stdout, strings.TrimSuffix(input, "\n") + ": command not found")
+  if err != nil {
+    fmt.Fprintln(os.Stderr, "Error reading input: ", err)
+  }
+
+  fmt.Println(strings.TrimSuffix(input, "\n") + ": command not found")
 }
