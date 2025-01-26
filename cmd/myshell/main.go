62dc74335ba1b5a55788ddc9bbe2015788f2b82d
Evgenii Klepilin <evgenii.klepilin@gmail.com>
2025-01-25 18:14:24 -0600

    Add type builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,6 +11,11 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
+var BUILTINS = map[string]bool{
+  "echo": true,
+  "exit": true,
+}
+
 func main() {
   for {
     fmt.Fprint(os.Stdout, "$ ")
@@ -42,10 +47,17 @@ func main() {
           os.Exit(0)
         }  
       case "echo":
-        if len(arguments) < 1 {
-          fmt.Fprintln(os.Stderr, "Nothing to echo")
+        fmt.Println(strings.Join(arguments, " "))
+      case "type":
+        if len(arguments) != 1 {
+          fmt.Fprintln(os.Stderr, "Invalid arguments")
+          continue
+        }
+        _, present := BUILTINS[arguments[0]]
+        if present {
+          fmt.Println(arguments[0] + " is a shell builtin")
         } else {
-          fmt.Println(strings.Join(arguments, " "))
+          fmt.Println(arguments[0] + ": not found")
         }
       default:
         fmt.Println(strings.TrimSuffix(input, "\n") + ": command not found")
